1a96a971eed7f7340f1f6a9b89fcf8e835d98e86
Krishan <thegreatmaster550@gmail.com>
2023-05-06 21:18:24 +0545

    first commit

---
+++ app/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    USER = (
+        (1,'HOD'),
+        (2,'STAFF'),
+        (3,'STUDENT'),
+
+    )
+
+
+    user_type = models.CharField(choices=USER, max_length=50,default=1)
+    profile_pic = models.ImageField(upload_to='media/profile_pic')
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Session_Year(models.Model):
+    session_start = models.CharField(max_length=100)
+    session_end = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.session_start + " To " + self.session_end
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    address = models.TextField()
+    gender = models.CharField(max_length=100)
+    course_id = models.ForeignKey(Course,on_delete=models.DO_NOTHING)
+    session_year_id = models.ForeignKey(Session_Year,on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.admin.first_name + " " + self.admin.last_name
+
+class Staff(models.Model):
+    admin = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    address = models.TextField()
+    gender = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    Update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.admin.username
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    Update_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
---
+++ college_management_system/urls.py
@@ -0,0 +1,89 @@
+"""college_management_system URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .import views,Hod_Views,Staff_Views,Student_Views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/',views.BASE,name='base'),
+
+    # login path
+    path('',views.LOGIN,name='login'),
+    path('doLogin',views.doLogin,name='doLogin'),
+    path('doLogout',views.doLogout,name='doLogout'),
+
+
+
+    # Profile Update
+    path('Profile',views.PROFILE,name='profile'),
+    path('Profile/update',views.PROFILE_UPDATE,name='profile_update'),
+
+
+
+    # This is Hod Panel Url
+    path('Hod/Home',Hod_Views.HOME,name='hod_home'),
+    path('Hod/Student/Add',Hod_Views.ADD_STUDENT,name='add_student'),
+    path('Hod/Student/View',Hod_Views.VIEW_STUDENT,name='view_student'),
+    path('Hod/Student/Edit/<str:id>',Hod_Views.EDIT_STUDENT,name='edit_student'),
+    path('Hod/Student/Update',Hod_Views.UPDATE_STUDENT,name='update_student'),
+    path('Hod/Student/Delete/<str:admin>',Hod_Views.DELETE_STUDENT,name='delete_student'),
+
+
+
+    path('Hod/Staff/Add',Hod_Views.ADD_STAFF,name='add_staff'),
+    path('Hod/Staff/View',Hod_Views.VIEW_STAFF,name='view_staff'),
+    path('Hod/Staff/Edit/<str:id>',Hod_Views.EDIT_STAFF,name='edit_staff'),
+    path('Hod/Staff/Update',Hod_Views.UPDATE_STAFF,name='update_staff'),
+    path('Hod/Staff/Delete/<str:admin>',Hod_Views.DELETE_STAFF,name='delete_staff'),
+
+
+
+
+    path('Hod/Course/Add',Hod_Views.ADD_COURSE,name='add_course'),
+    path('Hod/Course/View',Hod_Views.VIEW_COURSE,name='view_course'),
+    path('Hod/Course/Edit/<str:id>',Hod_Views.EDIT_COURSE,name='edit_course'),
+    path('Hod/Course/Update',Hod_Views.UPDATE_COURSE,name='update_course'),
+    path('Hod/Course/Delete/<str:id>',Hod_Views.DELETE_COURSE,name='delete_course'),
+
+
+    path('Hod/Subject/Add',Hod_Views.ADD_SUBJECT,name='add_subject'),
+    path('Hod/Subject/View',Hod_Views.VIEW_SUBJECT,name='view_subject'),
+    path('Hod/Subject/Edit/<str:id>',Hod_Views.EDIT_SUBJECT,name='edit_subject'),
+    path('Hod/Subject/Update',Hod_Views.UPDATE_SUBJECT,name='update_subject'),
+    path('Hod/Subject/Delete/<str:id>',Hod_Views.DELETE_SUBJECT,name='delete_subject'),
+
+
+    path('Hod/Session/Add',Hod_Views.ADD_SESSION,name='add_session'),
+    path('Hod/Session/View',Hod_Views.VIEW_SESSION,name='view_session'),
+    path('Hod/Session/Edit/<str:id>',Hod_Views.EDIT_SESSION,name='edit_session'),
+    path('Hod/Session/Update',Hod_Views.UPDATE_SESSION,name='update_session'),
+    path('Hod/Session/Delete/<str:id>',Hod_Views.DELETE_SESSION,name='delete_session'),
+
+
+
+
+
+
+
+
+
+
+
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
